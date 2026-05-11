8669932c49bbe099b591e2a5c0e869b2c0c08fdf
tazerface007 <rohandesoza@duck.com>
2026-05-11 11:11:12 +0530

    Update Todo schema

--- app/database/models/todo.py
+++ app/database/models/todo.py
@@ -1,11 +1,15 @@
-from app.database import Base
+from datetime import datetime
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
-
+from app.database import Base
 
 class Todo(Base):
     __tablename__ = 'todos'
 
-    id:Mapped[int] = mapped_column(primary_key=True)
-    title:Mapped[str] = mapped_column(comment="Title of the Todo", nullable=False)
-    description:Mapped[str] = mapped_column(comment="Detailed description of the ", nullable=True)
-
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str | None] = mapped_column(nullable=True)
+    is_completed: Mapped[bool] = mapped_column(default=False)
+    is_archived: Mapped[bool] = mapped_column(default=False)
+    # Automatically sets the time when a row is created
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
